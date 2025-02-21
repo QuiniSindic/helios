27265009ffb8a038e0c29aec8e0b83a4503747bd
Elian <elian_fl@hotmail.com>
2025-02-21 01:20:14 +0100

    fix: config images

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,12 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
     remotePatterns: [
       {
-        hostname: "api.sofascore.app",
-        protocol: "https",
+        hostname: 'sofascore.app',
+        protocol: 'https',
       },
     ],
   },
