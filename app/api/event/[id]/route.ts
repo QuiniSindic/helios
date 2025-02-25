77f21c1c99e0f7fe3ed23b3255c4e87f4438851c
Elian <elian_fl@hotmail.com>
2025-02-25 11:29:18 +0100

    headers 2

--- app/api/event/[id]/route.ts
+++ app/api/event/[id]/route.ts
@@ -5,6 +5,19 @@ import { NextRequest, NextResponse } from 'next/server';
 async function getEventById(id: string) {
   const res = await axios.get<Match>(
     `https://api.sofascore.app/api/v1/event/${id}`,
+    {
+      headers: {
+        'User-Agent':
+          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36',
+        // cors: 'no-cors',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Headers':
+          'Content-Type, Authorization, X-Requested-With',
+        'Access-Control-Allow-Credentials': 'true',
+        'Content-Type': 'application/json',
+      },
+    },
     // https://www.sofascore.com/api/v1/event/{id}
   );
   const data = res.data;
