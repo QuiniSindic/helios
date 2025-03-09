68e624b5cab826e7e6042958842d92b102f54fde
Elian <elian_fl@hotmail.com>
2025-03-09 19:55:59 +0100

    with fetch

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -1,19 +1,16 @@
 import { THE_ODDS_API_KEY, THE_ODDS_API_URL } from '@/core/config';
 import { TheOddsEvent } from '@/types/the_odds/the_odds.types';
 import { addCrests, normalizeTeams } from '@/utils/the_odds.utils';
-import axios from 'axios';
 import { NextResponse } from 'next/server';
 
 export async function GET() {
   try {
-    const response = await axios.get(
+    const response = await fetch(
       `${THE_ODDS_API_URL}/sports/soccer_spain_la_liga/events?apiKey=${THE_ODDS_API_KEY}`,
     );
 
-    if (response.status !== 200) {
-    }
-
-    const events: TheOddsEvent[] = response.data;
+    const data = await response.json();
+    const events: TheOddsEvent[] = data.data;
 
     const normalizedEvents = normalizeTeams(events);
     const eventsWithCrests = addCrests(normalizedEvents);
@@ -23,6 +20,7 @@ export async function GET() {
     return;
   } catch (error) {
     console.log('error =>', error);
+    console.log({ error });
     return NextResponse.json(
       { error: 'Error fetching events' },
       { status: 500 },
