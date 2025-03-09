32328918ef696c2e35bc43a072d80023346dd283
Elian <elian_fl@hotmail.com>
2025-03-09 20:19:34 +0100

    without function

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -1,22 +1,39 @@
-import { getTodayBasketballEvents } from '@/services/basketball/basketballEvents';
-import { parseEventDataToSave, sortEvents } from '@/services/events.service';
-import { getTodayFootballEvents } from '@/services/football/footballEvents';
+import { parseEventDataToSave } from '@/services/events.service';
 import { ParsedEvent } from '@/types/sofascoreTypes/parsedEvents.types';
+import { parseFootballEventsByLeague } from '@/utils/sofascore/football.utils';
 import { createClient } from '@/utils/supabase/server';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET() {
   try {
-    const footballEvents = await getTodayFootballEvents();
-    const basketballEvents = await getTodayBasketballEvents();
-    console.log('footballEvents ==>', footballEvents);
-    console.log('basketballEvents ==>', basketballEvents);
+    const todayDate = new Date().toISOString().split('T')[0];
 
-    const sortedEvents = sortEvents(footballEvents, basketballEvents);
+    const response = await fetch(
+      `https://www.sofascore.com/api/v1/sport/football/scheduled-events/${todayDate}`,
+    );
 
-    if (sortedEvents.length === 0) {
-      return NextResponse.json({ sortedEvents: [] }, { status: 200 });
+    if (!response.ok) {
+      throw new Error('Error al obtener los eventos de hoy');
     }
+
+    // Extraemos la data de la API correctamente
+    const eventsData = await response.json();
+    console.log('Eventos de hoy:', eventsData);
+    const parsedEventsByLeague = parseFootballEventsByLeague(eventsData);
+    const sortedEvents = parsedEventsByLeague.sort(
+      (a, b) => a.startTimestamp - b.startTimestamp,
+    );
+
+    // const footballEvents = await getTodayFootballEvents();
+    // const basketballEvents = await getTodayBasketballEvents();
+    // console.log('footballEvents ==>', footballEvents);
+    // console.log('basketballEvents ==>', basketballEvents);
+
+    // const sortedEvents = sortEvents(footballEvents, basketballEvents);
+
+    // if (sortedEvents.length === 0) {
+    //   return NextResponse.json({ sortedEvents: [] }, { status: 200 });
+    // }
 
     return NextResponse.json({ sortedEvents }, { status: 200 });
   } catch (error) {
