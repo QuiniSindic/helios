33d4f18f48e36db9a5fedc35f78ee85ce7687276
Elian <elian_fl@hotmail.com>
2025-03-16 21:25:55 +0100

    log

--- app/(site)/predictions/page.tsx
+++ app/(site)/predictions/page.tsx
@@ -1,15 +1,17 @@
 export default async function PredictionsPage() {
   const data = await fetch(
     'https://www.sofascore.com/api/v1/sport/basketball/scheduled-events/2025-03-16',
-  );
-  const games = await data.json();
-  console.log('games =>', games);
+  )
+    .then((res) => res.json())
+    .catch((err) => console.error(err));
+  //   const games = await data.json();
+  //   console.log('games =>', games);
   return (
     <div>
       <h1>Predictions Page</h1>
-      {games.events[0].tournament.name} {''}
-      {games.events[0].homeTeam.name} vs {games.events[0].awayTeam.name} {''}
-      {games.events[0].homeScore.current} vs {games.events[0].awayScore.current}
+      {data.events[0].tournament.name} {''}
+      {data.events[0].homeTeam.name} vs {data.events[0].awayTeam.name} {''}
+      {data.events[0].homeScore.current} vs {data.events[0].awayScore.current}
     </div>
   );
 }
