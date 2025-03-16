9522af370784f511f74ecf8bdbb8782c2360e677
Elian <elian_fl@hotmail.com>
2025-03-16 16:06:15 +0100

    feat: headers

--- app/api/event/[slug]/route.ts
+++ app/api/event/[slug]/route.ts
@@ -10,8 +10,31 @@ export async function GET(
     const { slug } = await params;
     console.log('estamos dentro ok');
 
+    // const response = await fetch(
+    //   `https://apim.laliga.com/webview/api/web/matches/${slug}?contentLanguage=es&countryCode=ES&subscription-key=ee7fcd5c543f4485ba2a48856fc7ece9`,
+    // );
+
     const response = await fetch(
-      `https://apim.laliga.com/webview/api/web/matches/${slug}?contentLanguage=es&countryCode=ES&subscription-key=ee7fcd5c543f4485ba2a48856fc7ece9`,
+      `https://apim.laliga.com/webview/api/web/matches/${slug}?contentLanguage=es&countryCode=GB&subscription-key=ee7fcd5c543f4485ba2a48856fc7ece9`,
+      {
+        headers: {
+          accept: 'application/json, text/plain, */*',
+          'accept-language': 'es-ES,es;q=0.9',
+          'ocp-apim-subscription-key': 'ee7fcd5c543f4485ba2a48856fc7ece9',
+          priority: 'u=1, i',
+          'sec-ch-ua':
+            '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
+          'sec-ch-ua-mobile': '?0',
+          'sec-ch-ua-platform': '"Windows"',
+          'sec-fetch-dest': 'empty',
+          'sec-fetch-mode': 'cors',
+          'sec-fetch-site': 'same-site',
+          Referer: 'https://www.laliga.com/',
+          'Referrer-Policy': 'strict-origin-when-cross-origin',
+        },
+        body: null,
+        method: 'GET',
+      },
     );
 
     console.log('response =>', response);
