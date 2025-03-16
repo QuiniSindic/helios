84f2c4cdb680a2549f6f87f06b97c0111b0692d2
Elian <elian_fl@hotmail.com>
2025-03-16 16:22:56 +0100

    fix: changed fetch

--- app/(site)/event/[slug]/page.tsx
+++ app/(site)/event/[slug]/page.tsx
@@ -18,7 +18,26 @@ export default async function EventDetailPage({
   console.log('vercel url', process.env.VERCEL_URL);
 
   const res = await fetch(
-    `https://${process.env.VERCEL_URL}/api/event/${slug}`,
+    `https://apim.laliga.com/webview/api/web/matches/${slug}?contentLanguage=es&countryCode=GB&subscription-key=ee7fcd5c543f4485ba2a48856fc7ece9`,
+    {
+      headers: {
+        accept: 'application/json, text/plain, */*',
+        'accept-language': 'es-ES,es;q=0.9',
+        'ocp-apim-subscription-key': 'ee7fcd5c543f4485ba2a48856fc7ece9',
+        priority: 'u=1, i',
+        'sec-ch-ua':
+          '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-site',
+        Referer: 'https://www.laliga.com/',
+        'Referrer-Policy': 'strict-origin-when-cross-origin',
+      },
+      body: null,
+      method: 'GET',
+    },
   );
 
   console.log('res =>', res);
