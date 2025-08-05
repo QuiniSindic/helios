86cdef0aed2cadecd477f6a567fe769a6ce8574d
Elian <elian_fl@hotmail.com>
2025-08-05 22:26:10 +0200

    config fix

--- core/config.ts
+++ core/config.ts
@@ -9,11 +9,11 @@ export const QUINIELA_DATA_URL =
 
 export const PROJECT_ID = process.env.NEXT_PUBLIC_SUPABASE_PROJECT_ID || '';
 
-export const BACKEND_URL = process.env.BACKEND_URL || 'http://localhost:3001';
+export const BACKEND_URL =
+  process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:3001';
 
 export const API_LOGO_COMPETITION_URL =
   'https://lsm-static-prod.livescore.com/high/';
 
 export const API_LOGO_COMPETITION_URL_LOW =
   'https://lsm-static-prod.livescore.com/medium/';
-
